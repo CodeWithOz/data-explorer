acf3dab85b2283d1a1a6c076aa6958066085e76b
Uche Ozoemena <codewithoz@outlook.com>
2022-12-20 09:22:38 +0100

    Link to the source code on github

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,14 +35,12 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-        {/* TODO: add github repo here */}
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+        Source code available on <a
+          href="https://github.com/CodeWithOz/data-explorer"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
+          GitHub
         </a>
       </footer>
     </div>
